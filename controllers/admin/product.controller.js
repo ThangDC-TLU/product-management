debe3f3aa0747047d49116dce7d0f9ad37630557
ThangDC-TLU <dinhcaothang08102004@gmail.com>
2025-11-20 17:06:46 +0700

    pagination for product

--- controllers/admin/product.controller.js
+++ controllers/admin/product.controller.js
@@ -1,6 +1,7 @@
 const Product = require("../../model/product.model");
 const filterStatusHelper = require("../../helpers/filterStatus");
 const searchHelper = require("../../helpers/search");
+const paginationHelper = require("../../helpers/pagination")
 
 //[GET] /admin/products
 module.exports.product = async (req, res) => {
@@ -21,12 +22,36 @@ module.exports.product = async (req, res) => {
         find.title = objectSearch.regex;
     }
 
-    const products = await Product.find(find);
+    //Pagination
+    const countProducts = await Product.countDocuments(find);
+    let objectPagination = paginationHelper(
+        {
+            currentPage: 1,
+            limitItem: 4
+        }, 
+        req.query, 
+        countProducts
+    );
+
+    // if(req.query.page){
+    //     objectPagination.currentPage = parseInt(req.query.page);
+    // }
+
+    // objectPagination.skip = ((objectPagination.currentPage) - 1)*objectPagination.limitItem;
+    // console.log(objectPagination.skip);
+
+    // const countProducts = await Product.countDocuments(find);
+    // const totalPage = Math.ceil(countProducts/objectPagination.limitItem);
+    // objectPagination.totalPages = totalPage;
+    //End pagination
+
+    const products = await Product.find(find).limit(objectPagination.limitItem).skip(objectPagination.skip);
 
     res.render("admin/pages/product/index", {
         pageTitle: "Danh sách sản phẩm",
         products: products, 
         filterStatus: filterStatus, 
-        keyword: objectSearch.keyword
+        keyword: objectSearch.keyword,
+        pagination: objectPagination
     });
 };
