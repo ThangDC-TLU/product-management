f8834bf870449a8783661594081014063836246f
ThangDC-TLU <dinhcaothang08102004@gmail.com>
2025-11-20 15:19:17 +0700

    code filter and search

---
+++ public/admin/js/script.js
@@ -0,0 +1,39 @@
+// Button Status
+const buttonsStatus = document.querySelectorAll("[button-status]");
+console.log(buttonsStatus);
+if(buttonsStatus.length > 0){
+    let url = new URL(window.location.href);
+    buttonsStatus.forEach((button) => {
+        button.addEventListener("click", () => {
+            const status = button.getAttribute("button-status");
+            console.log(status);
+
+            if(status){
+                url.searchParams.set("status", status);
+            } else{
+                url.searchParams.delete("status");
+            }
+
+            window.location.href = url.href;
+        })
+    })
+}
+
+//Search
+const formSearch = document.querySelector("#form-search");
+if(formSearch){
+    let url = new URL(window.location.href);
+    // console.log(url);
+    formSearch.addEventListener("submit", e =>{
+        e.preventDefault();
+        const keyword = e.target.elements.keyword.value;
+        // console.log(keyword);
+        if(keyword){
+            url.searchParams.set("keyword", keyword);
+        } else{
+            url.searchParams.delete("keyword");
+        }
+
+        window.location.href = url.href;
+    })
+}
