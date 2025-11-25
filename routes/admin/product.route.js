f36fc6148a24232b8b9d9e10174847ad4ae918c4
ThangDC-TLU <dinhcaothang08102004@gmail.com>
2025-11-25 23:05:33 +0700

    upload image to the cloud

--- routes/admin/product.route.js
+++ routes/admin/product.route.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const router = express.Router();
 const multer = require("multer");
-const storageMulter = require("../../helpers/storageMulter");
-const upload = multer({ storage: storageMulter() });
+// const storageMulter = require("../../helpers/storageMulter");
+const uploadCloud = require("../../middlewares/admin/uploadCloud.middelware");
+
+const upload = multer();
 const controller = require("../../controllers/admin/product.controller");
-const validate = require("../../validates/admin/product.validate")
+const validate = require("../../validates/admin/product.validate");
 
 router.get("/", controller.product);
 router.patch("/change-status/:status/:id", controller.changeStatus);
@@ -12,17 +14,20 @@ router.patch("/change-multi", controller.changeMulti);
 router.delete("/delete/:id", controller.deleteProduct);
 router.get("/create", controller.create);
 router.post(
-    "/create", 
-    upload.single('thumbnail'),
-    validate.createProduct,
-    controller.createProduct);
+  "/create",
+  upload.single("thumbnail"),
+  uploadCloud.upload,
+  validate.createProduct,
+  controller.createProduct
+);
 
 router.get("/edit/:id", controller.edit);
 router.patch(
-    "/edit/:id",
-    upload.single('thumbnail'),
-    validate.createProduct, 
-    controller.editProduct);
+  "/edit/:id",
+  upload.single("thumbnail"),
+  validate.createProduct,
+  controller.editProduct
+);
 
 router.get("/detail/:id", controller.detail);
 module.exports = router;
