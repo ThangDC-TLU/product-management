8344e88d580ed17f5fe92b5229f9ee565c4c87e8
ThangDC-TLU <dinhcaothang08102004@gmail.com>
2025-11-25 21:11:59 +0700

    xem chi tiết client

--- controllers/clients/product.controller.js
+++ controllers/clients/product.controller.js
@@ -9,10 +9,27 @@ module.exports.index = async (req, res) => {
         item.priceNew = (item.price * (100 - item.discountPercentage)/100).toFixed(0);
         return item;
     })
-    console.log(products);
     res.render("client/pages/products/index", {
         pageTitle: "Danh sách sản phẩm",
         products: newProducts
         
     });
 };
+
+module.exports.detail = async (req, res) => {
+    try {
+        const find = {
+            deleted: false,
+            slug: req.params.slug,
+            status: "active"
+        }
+        const productCurrent = await Product.findOne(find);
+        console.log(productCurrent)
+        res.render("client/pages/products/detail", {
+            pageTitle: productCurrent.title, 
+            product: productCurrent
+        });
+    } catch (error) {
+        res.redirect(`/products`)
+    }
+}
